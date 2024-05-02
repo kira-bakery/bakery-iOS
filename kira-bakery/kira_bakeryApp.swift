379368f75970c8f0e3b42df34bfab1efa3500e99
sio2whocodes <fltcy2039@naver.com>
2024-05-02 14:54:56 +0900

    Initial Commit

---
+++ kira-bakery/kira_bakeryApp.swift
@@ -0,0 +1,20 @@
+//
+//  kira_bakeryApp.swift
+//  kira-bakery
+//
+//  Created by sujeong on 2024/05/02.
+//
+
+import SwiftUI
+
+@main
+struct kira_bakeryApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ kira-bakeryUITests/kira_bakeryUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  kira_bakeryUITestsLaunchTests.swift
+//  kira-bakeryUITests
+//
+//  Created by sujeong on 2024/05/02.
+//
+
+import XCTest
+
+final class kira_bakeryUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
